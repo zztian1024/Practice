07b4a16fd438e629efbe8e89b489eabedc97a561
zhenzi <tianzhenzi@inke.cn>
2021-03-08 13:38:15 +0800

    Add NSDictionary+NWSafe

---
+++ Utility/Utility/Objc/NSDictionary+NWSafe.h
@@ -0,0 +1,21 @@
+//
+//  NSDictionary+NilSafe.h
+//  NSDictionary-NilSafe
+//
+//  Created by Allen Hsu on 6/22/16.
+//  Copyright © 2016 Glow Inc. All rights reserved.
+// https://github.com/allenhsu/NSDictionary-NilSafe
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDictionary (NWSafe)
+
+@end
+
+@interface NSMutableDictionary (NWSafe)
+
+@end
+
+NS_ASSUME_NONNULL_END
